98124c8407a4482dc50dec2175f91823464856e2
Jay Carreon <jay.carreon@embedcard.com>
2020-04-21 03:39:06 +0800

    Updates

---
+++ precache-manifest.b0a5f26ef24d8ee033a63e97f4c40500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9858415e1d707b87160e94e197ed1af3",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "06666bf21ac55c9abe19",
+    "url": "/react-project/static/css/main.bf187180.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/react-project/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react-project/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06666bf21ac55c9abe19",
+    "url": "/react-project/static/js/main.b64fb41a.chunk.js"
+  },
+  {
+    "revision": "388565225f11edf6a2a6",
+    "url": "/react-project/static/js/runtime-main.4ee3e109.js"
+  }
+]);
